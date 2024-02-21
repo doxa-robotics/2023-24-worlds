2bb95ec15f27e8c5359ef5a3f11a1f1ac536d24a
rh0820 <127829015+rh0820@users.noreply.github.com>
2024-02-21 16:29:53 +0900

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       Programer                                                    #
+# 	Created:      2/21/2024, 4:15:13 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
