12a7b244d986779639626fa07845c84a92098049
rh0820 <127829015+rh0820@users.noreply.github.com>
2024-02-28 16:39:39 +0900

    add controller damping

--- src/driver_control.py
+++ src/driver_control.py
@@ -4,20 +4,29 @@ from ultrasonic_claw import UltrasonicClaw
 from autonomous_common import debug
 
 
+def _damp_controller(val):
+    value = math.pow(0.1*val, 2)
+    if val < 0:
+        return -value
+    else:
+        return value
+
+
 def driver_control(p: Peripherals):
     last_a_pressing = False
     last_b_pressing = False
-    last_y_pressing = False
     ultrasonic_claw = UltrasonicClaw(p.claw_piston, p.front_sonar)
     while True:
         # Spin the left and right groups based on the controller
         axis3 = p.controller.axis3.position()
-        if axis3 < 3 and axis3 > -3:
+        if -5 < axis3 < 5:
             axis3 = 0
+        axis3 = _damp_controller(axis3)
 
         axis1 = p.controller.axis1.position()
-        if axis1 < 3 and axis1 > -3:
+        if -5 < axis1 < 5:
             axis1 = 0
+        axis1 = _damp_controller(axis1)
 
         p.left_motors.spin(
             DirectionType.FORWARD,
