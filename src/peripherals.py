c09b3d04f65bbdd65d1d2a1b3e08de8aba729d41
rh0820 <127829015+rh0820@users.noreply.github.com>
2024-02-29 17:28:04 +0900

    update perfs

--- src/peripherals.py
+++ src/peripherals.py
@@ -19,26 +19,24 @@ class RealBotPeripherals(Peripherals):
         self.brain = Brain()
         self.controller = Controller()
 
-        self.wing_piston = Pneumatics(self.brain.three_wire_port.f)
-        self.claw_piston = Pneumatics(self.brain.three_wire_port.a)
+        self.wing_piston = Pneumatics(self.brain.three_wire_port.g)
+        self.claw_piston = Pneumatics(self.brain.three_wire_port.h)
 
         self.front_sonar = Sonar(self.brain.three_wire_port.c)
 
-        self.inertial = Inertial(Ports.PORT5)
+        self.inertial = Inertial(Ports.PORT6)
 
         self.left_motors = MotorGroup(
-            Motor(Ports.PORT18, False),
-            Motor(Ports.PORT10, True),
-
+            Motor(Ports.PORT20, False),
             Motor(Ports.PORT19, True),
             Motor(Ports.PORT9, False),
+            Motor(Ports.PORT8, True),
         )
         self.right_motors = MotorGroup(
-            Motor(Ports.PORT11, True),
-            Motor(Ports.PORT1, False),
-
-            Motor(Ports.PORT17, False),
-            Motor(Ports.PORT2, True),
+            Motor(Ports.PORT12, True),
+            Motor(Ports.PORT13, False),
+            Motor(Ports.PORT1, True),
+            Motor(Ports.PORT2, False),
         )
         self.drivetrain = SmartDrive(
             self.left_motors,
