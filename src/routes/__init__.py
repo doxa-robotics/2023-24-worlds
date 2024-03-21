e881b43699f4d61be075822862c9a10d50f49072
Spaceyhula <150232198+Spaceyhula@users.noreply.github.com>
2024-03-21 16:40:04 +0900

    add o2 route to list

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -2,7 +2,14 @@ from _route import EmptyRoute, Route
 from route_o1 import O1Route
 from route_test import TestRoute
 from route_d1 import D1Route
+from route_o2 import O2Route
 
-routes: list[type[Route]] = [O1Route, D1Route, TestRoute, EmptyRoute]
+routes: list[type[Route]] = [
+    O1Route,
+    O2Route,
+    D1Route,
+    TestRoute,
+    EmptyRoute
+]
 
 __all__ = ["Route", "routes"]
