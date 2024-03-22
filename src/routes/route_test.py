16abf7c120389a27bf35257b6aa81a87cb88cc40
zabackary <137591653+zabackary@users.noreply.github.com>
2024-03-22 16:52:34 +0900

    start fiddling with PID gains for driving

--- src/routes/route_test.py
+++ src/routes/route_test.py
@@ -9,4 +9,7 @@ class TestRoute(TestRouteCategory):
 
     @staticmethod
     def run(p, d):
-        d.drive(300)
+        d.drive(1000)
+        p.brain.screen.set_fill_color(0xff0000)
+        p.brain.screen.draw_rectangle(0, 0, 500, 500)
+        p.brain.screen.render()
