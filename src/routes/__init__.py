e0bf0fdef13403e79889a5b0dab4b710050b1b61
zabackary <137591653+zabackary@users.noreply.github.com>
2024-04-01 16:55:50 +0900

    Add prototype d3 route and update list

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,7 +1,10 @@
 from _route import EmptyRoute, Route
-from route_o1 import O1Route
+
 from route_test import TestRoute
 from route_d1 import D1Route
+from route_d2 import D2Route
+from route_d3 import D3Route
+from route_o1 import O1Route
 from route_o2 import O2Route
 from route_o3 import O3Route
 
@@ -10,6 +13,8 @@ routes: list[type[Route]] = [
     O2Route,
     O3Route,
     D1Route,
+    D2Route,
+    D3Route,
     TestRoute,
     EmptyRoute
 ]
