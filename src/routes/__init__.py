f9052b255048a4729b1999d4673e0bc903509b16
zabackary <137591653+zabackary@users.noreply.github.com>
2024-04-01 16:33:49 +0900

    Add offense 3

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -3,10 +3,12 @@ from route_o1 import O1Route
 from route_test import TestRoute
 from route_d1 import D1Route
 from route_o2 import O2Route
+from route_o3 import O3Route
 
 routes: list[type[Route]] = [
     O1Route,
     O2Route,
+    O3Route,
     D1Route,
     TestRoute,
     EmptyRoute
