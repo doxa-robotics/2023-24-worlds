99b2eb88429278479da3bb329f1a78dadfc3500f
zabackary <137591653+zabackary@users.noreply.github.com>
2024-04-02 16:47:46 +0900

    add d4 to route listing

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -4,6 +4,7 @@ from route_test import TestRoute
 from route_d1 import D1Route
 from route_d2 import D2Route
 from route_d3 import D3Route
+from route_d4 import D4Route
 from route_o1 import O1Route
 from route_o2 import O2Route
 from route_o3 import O3Route
@@ -11,14 +12,17 @@ from route_o4 import O4Route
 from route_o5 import O5Route
 
 routes: list[type[Route]] = [
+    D1Route,
+    D2Route,
+    D3Route,
+    D4Route,
+
     O1Route,
     O2Route,
     O3Route,
     O4Route,
     O5Route,
-    D1Route,
-    D2Route,
-    D3Route,
+
     TestRoute,
     EmptyRoute
 ]
--- src/routes/route_d4.py
+++ src/routes/route_d4.py
@@ -1,26 +1,22 @@
-from _route import OffenseRoute
-from vex import wait
+from _route import DefenseRoute
 
 
-class TestRoute(OffenseRoute):
+class D4Route(DefenseRoute):
+    """ Start facing goal, middle of the mat between the corner and the elevation bar """
     @staticmethod
     def name():
         return ""
 
-
-# Start facing goal, middle of the mat between the corner and the elevation bar
-
     @staticmethod
     def run(p, d):
         p.claw_piston.open()
-        d.drive (400)
+        d.drive(400)
         d.turn(25)
-        d.drive (500)
+        d.drive(500)
         p.claw_piston.close()
-        d.drive (-100)
+        d.drive(-100)
         p.wing_piston.open()
         d.turn(-200)
         d.drive(700)
         d.turn(120)
         d.drive(500)
-       
