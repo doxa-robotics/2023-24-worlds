de9e951a6cffbbf93d6c098942643e57bf85f3c5
zabackary <137591653+zabackary@users.noreply.github.com>
2024-04-02 17:46:54 +0900

    fix 05 based on real-world

--- src/routes/route_o5.py
+++ src/routes/route_o5.py
@@ -20,24 +20,30 @@ class O5Route(OffenseRoute):
 
         # drive backwards to descore match load zone
         d.drive(-400)
-        d.turn_to(90)
-        d.drive(-800)  # back into the goal
+        d.turn_to(100)
+        p.wing_piston.close()
+        d.drive(-1200)  # back into the goal
+        p.wing_piston.close()
         p.claw_piston.open()  # open the claw
-        d.turn_to(0)  # prepare to back out
+        d.turn_to(360 - 20)  # prepare to back out
 
         # back out of the goal and get the center and bar-center triballs
-        d.drive(-1200)
-        d.turn_to(190)  # turn around and approach at an angle
-        d.drive(500)
+        d.drive(-900)
+        d.turn_to(215)  # turn around and approach at an angle
+        d.drive(530)
         p.claw_piston.close()
+        d.turn_to(170)
+        p.wing_piston.open()
         d.drive(-800)  # back into the goal
 
-        return
-
         # deposit the captured triball
-        d.turn_to(360 - 15)
+        p.wing_piston.close()
+        d.drive(300)
+        d.turn(180)
         p.claw_piston.open()
-        d.drive(-600)
+        d.drive(500)
+
+        return
         d.turn_to(180 - 15)  # spin around to intake
         d.drive(500)
         p.claw_piston.close()
