770b733c82781bd71a1764ac9b60c27079d680b6
zabackary <137591653+zabackary@users.noreply.github.com>
2024-04-05 17:33:30 +0900

    Changrd to absolute heading d6

--- src/routes/route_d6.py
+++ src/routes/route_d6.py
@@ -10,33 +10,34 @@ class D6Route(DefenseRoute):
 
     @staticmethod
     def run(p, d):
-        p.inertial.set_heading(45)
+        p.inertial.set_heading(360 - 45)
 
         p.claw_piston.close()
         p.wing_piston.open()
-        d.turn(360)
+
+        d.turn(-360)
         p.wing_piston.close()
 
         # Going toward triball
         d.drive(700)
-        d.turn(40)
+        d.turn_to(-90)
         p.wing_piston.open()
         p.claw_piston.open()
         d.drive(900)
         p.claw_piston.close()
 
         # Go to the middle bar
-        d.turn(90)
+        d.turn_to(0)
         d.drive(700)
         p.wing_piston.open()
         p.wing_piston.close()
 
         # Go back to first place
-        d.turn(-40)
+        d.turn_to(-45)
         d.drive(1980)
 
         # Going to the goal
-        d.turn(47.5)
+        d.turn_to(-90)
         d.drive(1100)
 
         # Go back to the goal backward
@@ -49,6 +50,6 @@ class D6Route(DefenseRoute):
         d.drive(700)
         d.turn(-50)
         d.drive(600)
-        d.turn(-38)
+        d.turn_to(0)
         p.wing_piston.open()
         d.drive(1100)
