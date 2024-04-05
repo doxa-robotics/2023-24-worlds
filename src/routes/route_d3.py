654ad465ef70407a3ea5d5d022102223b04a525a
zabackary <137591653+zabackary@users.noreply.github.com>
2024-04-05 17:05:57 +0900

    finish d3

--- src/routes/route_d3.py
+++ src/routes/route_d3.py
@@ -10,14 +10,40 @@ class D3Route(DefenseRoute):
 
     @staticmethod
     def run(p, d):
-        p.inertial.set_heading(45)
+        # p.inertial.set_heading(180 + 45)
+        p.inertial.set_heading(360 - 45)
         p.wing_piston.open()
         p.claw_piston.close()
-        d.turn_to(0)
+
+        d.turn_to(180 + 20)  # descore
         p.wing_piston.close()
-        d.turn_to(240)
-        d.drive(700)
+
+        # prepare to score preload
+        d.turn_to(180 + 65)
         p.claw_piston.open()
-        d.drive(-700)
-        d.turn_to(20)
-        d.drive(1000)
+        d.drive(120)
+        p.wait(500)
+        for _ in range(8):
+            d.drive(45)
+
+        # back against the wall to reorient
+        p.wait(500)
+        d.turn_to(270)
+        d.drive(45)
+        d.drive(45)
+        d.drive(-200)
+        d.turn_to(0)
+        d.drive(-100)
+
+        # turn from the back and push the ball in
+        d.turn_to(90)
+        d.drive(-210)
+        d.drive(-55)
+
+        # go to the goal
+        d.drive(100)
+        d.turn_to(180 + 45)
+        d.drive(-800)
+        d.turn_to(180)
+        d.drive(-560)
+        d.drive(-70)
