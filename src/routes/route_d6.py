f8facff4e3cdde09838b792a9969fbd8d20e9a54
zabackary <137591653+zabackary@users.noreply.github.com>
2024-04-09 16:11:29 +0900

    kind of working d6

--- src/routes/route_d6.py
+++ src/routes/route_d6.py
@@ -12,44 +12,19 @@ class D6Route(DefenseRoute):
     def run(p, d):
         p.inertial.set_heading(360 - 45)
 
-        p.claw_piston.close()
-        p.wing_piston.open()
-
-        d.turn(-360)
-        d.turn_to(-60)
-        p.wing_piston.close()
-
-        # Going toward triball
-        d.drive(950)
+        d.drive(740)
+        d.turn_to(270)
+        d.drive(530)
         d.drive(50)
         p.claw_piston.close()
-
-        # Go to the middle bar
-        d.turn_to(-90)
-        p.claw_piston.open()
-        p.wing_piston.open()
-        d.drive(250)
-
-        # Go back to first place
-        d.turn_to(-45)
-
-        return
-        d.drive(1980)
-
-        # Going to the goal
-        d.turn_to(-90)
-        d.drive(1100)
-
-        # Go back to the goal backward
         d.drive(-100)
-        d.turn(180)
+        d.turn_to(180)
         p.wing_piston.open()
-        d.drive(100)
-
-        # Go to the bar
-        d.drive(700)
-        d.turn(-50)
-        d.drive(600)
+        d.drive(-280)
+        d.turn_to(270)
+        p.wing_piston.close()
         d.turn_to(0)
-        p.wing_piston.open()
-        d.drive(1100)
+        d.drive(-300)
+        p.claw_piston.open()
+        d.drive(1150)
+        d.drive(-1000)
