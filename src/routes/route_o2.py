d4fee1cd0cc67e7e65585b25f37f97a89dbeaab8
zabackary <137591653+zabackary@users.noreply.github.com>
2024-04-12 16:46:18 +0900

    delete o2 and o3 code

--- src/routes/route_o2.py
+++ src/routes/route_o2.py
@@ -2,59 +2,10 @@ from _route import OffenseRoute
 
 
 class O2Route(OffenseRoute):
-    """ An offensive auton route, based off of cachedVideo.mp4
-
-    slightly modified
-    """
     @staticmethod
     def name():
         return "Offense 2"
 
     @staticmethod
     def run(p, d):
-        p.claw_piston.open()
-        p.wing_piston.open()
-        d.drive(400)
-        p.wing_piston.close()
-        d.drive(700)
-        d.turn(-30)
-        d.drive(840)
-
-        p.claw_piston.close()
-        p.wing_piston.close()
-        d.drive(-1000)
-        d.turn(90)
-        p.claw_piston.open()
-        d.turn(170)
-        # Go get the ball under the bar
-        d.drive(600)
-        d.turn(48)
-        d.drive(800)
-        p.claw_piston.close()
-        d.drive(-1100)
-        d.turn(50)
-        p.wing_piston.open()
-        d.drive(500)
-        d.turn(45)
-        d.drive(660)
-        p.wing_piston.close()
-        d.drive(660)
-        d.turn(180)
-        d.drive(100)
-        d.drive(-100)
-
-        d.turn(90)
-        p.claw_piston.open()
-
-        d.drive(1680)
-
-        p.claw_piston.close()
-        d.turn(1000)
-        d.drive(1000)
-        d.turn(38)
-        d.drive(1000)
-
-        d.turn(148)
-        p.wing_piston.open()
-        p.claw_piston.open()
-        d.drive(1000)
+        pass
--- src/routes/route_o3.py
+++ src/routes/route_o3.py
@@ -11,24 +11,3 @@ class O3Route(OffenseRoute):
     @staticmethod
     def run(p, d):
         p.inertial.set_heading(225)
-        d.turn_to(230)
-        d.drive(1600)
-        # rush
-        p.claw_piston.close()
-        d.turn_to(180)
-        p.wing_piston.open()
-        # back into goal
-        d.drive(-800)
-        p.wing_piston.close()
-        d.drive(400)
-        # spin to de-intake
-        d.turn_to(0)
-        d.drive(400)
-        p.claw_piston.open()
-        # back out of goal
-        d.drive(-400)
-        # not getting 3rd triball due to time
-        d.turn_to(90)
-        d.drive(1600)
-        d.turn_to(0)
-        d.drive(-600)
