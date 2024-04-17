b72e9f5f784becd79d8b8ccced6041ac724e9be4
zabackary <137591653+zabackary@users.noreply.github.com>
2024-04-17 17:44:35 +0900

    d5 done!

--- src/routes/route_d5.py
+++ src/routes/route_d5.py
@@ -11,6 +11,7 @@ class D5Route(DefenseRoute):
         p.inertial.set_heading(90.0 + 8.2)
 
         p.wing_piston.open()
+        p.wait(500)
         d.drive(1090)
 
         # intake the middle ball
@@ -25,26 +26,29 @@ class D5Route(DefenseRoute):
         p.wing_piston.close()
 
         # get remaining three triballs
-        d.turn_to(120)
-        d.drive(-1200)
+        d.turn_to(125)
+        d.drive(-1180)
 
         # do corner
         p.wing_piston.open()
-        d.turn_to(180 + 45)
-        d.drive(-600)
-        p.wing_piston.open()
-        d.turn_to(270)
+        d.turn_to(180 + 35)
+        d.drive(-350)
+        d.turn_to(245)
         p.wing_piston.close()
-        d.drive(-100)
-        d.drive(-50)
+        d.drive(-300)
+        d.turn_to(270)
+        d.drive(-30)
         d.turn_to(270)
 
         # go descore
         d.drive(300)
         d.turn_to(45)
         p.wing_piston.open()
-        d.drive(-200)
-        d.turn_to(0)
-        d.drive(-1000)
-        d.drive(-100)
+        d.drive(-330)
+
+        # touch bar
+        d.turn_to(10)
+        d.drive(-830)
         p.wing_piston.close()
+        d.turn_to(0)
+        d.drive(-100)
