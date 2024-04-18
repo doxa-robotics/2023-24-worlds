9dfe34c861a4879fd26458ab5657b488073bcef3
zabackary <137591653+zabackary@users.noreply.github.com>
2024-04-18 16:09:13 +0900

    finish d5!

--- src/routes/route_d5.py
+++ src/routes/route_d5.py
@@ -31,25 +31,25 @@ class D5Route(DefenseRoute):
 
         # do corner
         p.wing_piston.open()
-        d.turn_to(180 + 35)
-        d.drive(-350)
-        d.turn_to(245)
-        d.drive(-250)
+        d.turn_to(180 + 25)
+        d.drive(-450)
+        d.turn_to(230)
+        d.drive(-240)
         p.wing_piston.close()
         d.turn_to(270)
-        d.drive(-150)
-        d.drive(-50)
+        d.drive(-200)
         d.turn_to(270)
 
         # go descore
-        d.drive(250)
+        d.drive(230)
+        d.drive(60)
         d.turn_to(45)
         p.wing_piston.open()
-        d.drive(-310)
+        d.drive(-360)
 
         # touch bar
         d.turn_to(10)
-        d.drive(-830)
+        d.drive(-860)
         p.wing_piston.close()
         d.turn_to(0)
         d.drive(-100)
