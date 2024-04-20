ba65b7a2bb2fabcbf28f3cf1ad5e86435c882960
zabackary <137591653+zabackary@users.noreply.github.com>
2024-04-20 10:45:50 +0900

    remove unused routes from the codebase

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,31 +1,18 @@
 from _route import EmptyRoute, Route
-
-from route_test import TestRoute
-from route_d1 import D1Route
-from route_d2 import D2Route
 from route_d3 import D3Route
-from route_d4 import D4Route
 from route_d5 import D5Route
-from route_d6 import D6Route
-from route_o1 import O1Route
 from route_o2 import O2Route
 from route_o3 import O3Route
-from route_o4 import O4Route
 from route_o5 import O5Route
 from route_o6 import O6Route
+from route_test import TestRoute
 
 routes: list[type[Route]] = [
-    # D1Route,
-    # D2Route,
     D3Route,
-    # D4Route,
     D5Route,
-    # D6Route,
 
-    # O1Route,
     O2Route,
     O3Route,
-    # O4Route,
     O5Route,
     O6Route,
 
